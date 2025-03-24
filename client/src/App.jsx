c82d5cf7ec32ca8e2efd48c4448903312a24d3cd
Erik Varga <viglew@gmail.com>
2025-03-24 08:06:54 -0400

    added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='px-4 md:px-8 lg:px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64'>
+    <div className='flex flex-col justify-between h-full px-4 md:px-8 lg:px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64 bg-gray-100'>
       {/* Navbar */}
+      <Navbar />
       {/* Breadcrumb */}
       {/* Introduction */}
       {/* Featured Posts */}
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaBars } from 'react-icons/fa';
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <div className='w-full h-16 md:h-20 flex items-center justify-between bg-gray-100'>
+        {/* logo */}
+        <div className="flex items-center gap-4 text-2xl font-bold">
+            <img src="/favicon.jpg" alt="logo" className='w-8 h-8 border-gray-900 rounded-sm shadow-md' />
+            <span>spudevlog</span>
+        </div>
+
+        {/* mobile */}
+        <div className="md:hidden">
+            {/* button */}
+            <div className="cursor-pointer text-2xl flex items-center justify-center"
+                onClick={() => setOpen((prev) => !prev)}
+            >
+                {open ? <IoClose /> : <FaBars />}
+            </div>
+
+            {/* links */}
+            <div className={`w-full h-screen flex flex-col items-center justify-center gap-8 text-lg bg-gray-100 absolute top-16 transition-all ease-in-out ${open ? "-right-0" : "-right-[100%]"}`}>
+                <a href="/">Home</a>
+                <a href="/">Trending</a>
+                <a href="/">Most Popular</a>
+                <a href="/">About</a>
+                <a href="/"><button className='py-2 px-4 rounded-2xl bg-blue-400 text-white'>Login</button></a>
+            </div>
+        </div>
+        {/* desktop menu */}
+        <div className="hidden md:flex items-center gap-8 xl:gap-12">
+            <a href="/">Home</a>
+            <a href="/">Trending</a>
+            <a href="/">Most Popular</a>
+            <a href="/">About</a>
+            <a href="/"><button className='py-2 px-4 rounded-2xl bg-blue-400 text-white'>Login</button></a>
+        </div>
+    </div>
   )
 }
 
