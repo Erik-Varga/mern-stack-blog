14e74ef61208f863b76d9e1a702823f59c349a1b
Erik Varga <viglew@gmail.com>
2025-03-25 08:18:43 -0400

    added react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,14 +9,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='flex flex-col justify-between h-full px-4 md:px-8 lg:px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64 bg-gray-100'>
+    <div>
       {/* Navbar */}
-      <Navbar />
       {/* Breadcrumb */}
       {/* Introduction */}
       {/* Featured Posts */}
       {/* Post List */}
-      <Footer />
+      
     </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,52 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from './routes/Homepage';
+import PostListPage from './routes/PostListPage';
+import Write from './routes/Write';
+import LoginPage from './routes/LoginPage';
+import RegisterPage from './routes/RegisterPage.jsx';
+import SinglePostPage from './routes/SinglePostPage';
+import MainLayout from './layouts/MainLayout.jsx';
+import ErrorPage from './routes/Errorpage.jsx';
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/posts",
+        element: <PostListPage />,
+      },
+      {
+        path: "/slug",
+        element: <SinglePostPage />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+    ]
+  }
+  
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
