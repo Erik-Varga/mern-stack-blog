a3400892a4a3ae0c80a95fbf2de7d2574f4fcad4
Erik Varga <viglew@gmail.com>
2025-03-30 07:49:51 -0400

    added mongodb and mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express"
+import connectDB from "./lib/connectDB.js"
 import userRouter from "./routes/user.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
@@ -10,5 +11,6 @@ app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 
 app.listen(5000, ()=> {
+    connectDB();
     console.log("Server is running!")
 })
